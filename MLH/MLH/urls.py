af3cb598341b792894bfc06c3aa9f625a05366b3
Gerson Antonio <gelsonantonio53@outlook.com>
2024-04-23 16:58:20 +0200

    started a new project where all teh apps are micro services

---
+++ MLH/MLH/urls.py
@@ -0,0 +1,18 @@
+"""
+URL configuration for MY_LEARNING_HUB project.
+
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from user_auth import views, urls
+from MLH import settings
+
+# added the static files to be able to load the files under the static folder
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include("base.urls")),
+    path('accounts/', include('allauth.urls')),
+    
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
